adb3133a548581172fdc343c79a1df7bbe9e2514
Артём Чудодеев <artem@chudodeev.ru>
2023-12-14 17:12:42 +0300

    рабочее приложение с единичным выбором ответа и без подробных логов тестирования

--- quizes/views.py
+++ quizes/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from .models import Quiz
 from django.views.generic import ListView
 from django.http import JsonResponse
-
+from questions.models import Question, Answer
+from results.models import Result
 
 class QuizListView(ListView):
     model = Quiz
@@ -26,3 +27,56 @@ def quiz_data_view(request, pk):
         'data': questions,
         'time': quiz.time,
     })
+
+
+def save_quiz_view(request, pk):
+    #print(request.POST)
+    questions = []
+    data = request.POST
+    data_ = dict(data.lists())
+    data_.pop('csrfmiddlewaretoken')
+    print(data_)
+
+    for k in data_.keys():
+        try:
+            question = Question.objects.get(text=k)
+            questions.append(question)
+        except:
+            None
+    print(questions)
+
+    user = request.user
+    quiz = Quiz.objects.get(pk=pk)
+
+    score = 0
+    multiplier = 100 / quiz.number_of_questions
+    results = []
+    correct_answer = None
+
+    for q in questions:
+        a_selected = request.POST.get(str(q.text))
+        print('selected -->', a_selected)
+
+        if a_selected != '':
+            question_answers = Answer.objects.filter(question=q)
+            for a in question_answers:
+                if a_selected == a.text:
+                    if a.correct:
+                        score += 1
+                        correct_answer = a.text
+                else:
+                    if a.correct:
+                        correct_answer = a.text
+            results.append({str(q): {'correct_answer': correct_answer, 'answered': a_selected}})
+        else:
+            results.append({str(q): 'not answered'})
+    
+    score_ = score * multiplier
+    Result.objects.create(quiz=quiz, user=user, score=score)
+
+    if score_ >= quiz.required_score_to_pass:
+        return JsonResponse({'passed': True, 'score': score_, 'results': results})
+    else:
+        return JsonResponse({'passed': False, 'score': score_, 'results': results})
+
+    return JsonResponse({'text': 'works'})
