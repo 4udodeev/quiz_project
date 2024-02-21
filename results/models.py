d09b5d9465e338360e890e87e74ebde482182b3c
4udo <4udodeev@gmail.com>
2024-02-21 11:56:20 +0300

    default_worked

--- results/models.py
+++ results/models.py
@@ -1,12 +1,18 @@
-from django.db import models
-from quizes.models import Quiz
+"""Models."""
 from django.contrib.auth.models import User
+from django.db import models
+
+from quizes.models import Quiz
 
 
 class Result(models.Model):
+    """Результаты тестирования."""
+
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     score = models.FloatField()
 
     def __str__(self) -> str:
-        return f'{self.user.last_name} {self.user.first_name} - {self.quiz.name}'
+        """Str."""
+        return (f'{self.user.last_name} {self.user.first_name} -'
+                f' {self.quiz.name}')
